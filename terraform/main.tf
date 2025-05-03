a812d63f3713194f0e1bbc19e2de5eb7c161beb8
Priti Mahadik <primahadik@192.168.1.107>
2025-05-03 22:26:02 +0100

    Add CI/CD workflow, Azure pipeline, Dockerfile, and Terraform code

---
+++ terraform/main.tf
@@ -0,0 +1,54 @@
+terraform {
+  required_providers {
+    azurerm = { source = "hashicorp/azurerm", version = "~> 3.0" }
+  }
+  backend "azurerm" {
+    resource_group_name  = "tfstate-rg"
+    storage_account_name = "tfstateadogithubactionterraform"
+    container_name       = "state"
+    key                  = "demo.terraform.tfstate"
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "rg" {
+  name     = var.rg_name
+  location = var.location
+}
+
+resource "azurerm_service_plan" "plan" {
+  name                = "asp-${var.rg_name}"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  sku_name            = "B1"          # £‑friendly tier
+  os_type             = "Linux"
+}
+
+resource "azurerm_container_registry" "acr" {
+  name                = "acr${var.acr_suffix}"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  sku                 = "Basic"
+  admin_enabled       = true
+}
+
+resource "azurerm_linux_web_app" "app" {
+  name                = "web-${var.rg_name}"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  service_plan_id     = azurerm_service_plan.plan.id
+
+  site_config {
+    linux_fx_version = "DOCKER|${azurerm_container_registry.acr.login_server}/${var.image_name}:latest"
+  }
+
+  app_settings = {
+    WEBSITES_PORT = "80"
+    DOCKER_REGISTRY_SERVER_URL      = "https://${azurerm_container_registry.acr.login_server}"
+    DOCKER_REGISTRY_SERVER_USERNAME = azurerm_container_registry.acr.admin_username
+    DOCKER_REGISTRY_SERVER_PASSWORD = azurerm_container_registry.acr.admin_password
+  }
+}
---
+++ terraform/variables.tf
@@ -0,0 +1,4 @@
+variable "rg_name"     { default = "adogithubtf-rg" }
+variable "location"    { default = "uksouth" }
+variable "acr_suffix"  { default = "adogithubtf" }
+variable "image_name"  { default = "ado-github-terraform-demo" }
