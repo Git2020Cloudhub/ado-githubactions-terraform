6a7e9e64a7e848946657f018a1149a3f19cfc94f
Priti Mahadik <mahadik_priti@yahoo.com>
2025-05-07 00:46:44 +0100

    revise8

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,8 +1,9 @@
-terraform {
-  backend "azurerm" {
-    resource_group_name  = "adogithubtf-rg"
-    storage_account_name = "tfstateadogithubaction"
-    container_name       = "state"
-    key                  = "demo.terraform.tfstate"
-  }
-}
+ terraform {
+   backend "azurerm" {
+     resource_group_name  = "adogithubtf-rg"
+-    storage_account_name = "tfstateadogithubaction"
++    storage_account_name = "tfstateadogithubtf"   # ← match storage.tf / Azure portal
+     container_name       = "state"
+     key                  = "demo.terraform.tfstate"
+   }
+ }
