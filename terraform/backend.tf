cfbcc7057a9c84b7ed4427c824ae000233607608
Priti Mahadik <mahadik_priti@yahoo.com>
2025-05-07 00:50:33 +0100

    revise8

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,9 +1,8 @@
- terraform {
-   backend "azurerm" {
-     resource_group_name  = "adogithubtf-rg"
--    storage_account_name = "tfstateadogithubaction"
-+    storage_account_name = "tfstateadogithubtf"   # ← match storage.tf / Azure portal
-     container_name       = "state"
-     key                  = "demo.terraform.tfstate"
-   }
- }
+terraform {
+  backend "azurerm" {
+    resource_group_name  = "adogithubtf-rg"
+    storage_account_name = "tfstateadogithubtf"   # must match storage.tf
+    container_name       = "state"
+    key                  = "demo.terraform.tfstate"
+  }
+}
--- terraform/storage.tf
+++ terraform/storage.tf
@@ -1,11 +1,10 @@
- resource "azurerm_storage_account" "state" {
-   name                = "tfstate${replace(var.rg_name, "/-/", "")}"
-   name                = "tfstate${var.acr_suffix}"  # → tfstateadogithubtf
-   resource_group_name = azurerm_resource_group.rg.name
-   location            = azurerm_resource_group.rg.location
-   account_tier        = "Standard"
-   account_replication_type = "LRS"
- }
+resource "azurerm_storage_account" "state" {
+  name                     = "tfstate${var.acr_suffix}"  # → tfstateadogithubtf
+  resource_group_name      = azurerm_resource_group.rg.name
+  location                 = azurerm_resource_group.rg.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+}
 
 resource "azurerm_storage_container" "state" {
   name                  = "state"
