7142255f99e5d2e636b43fc831a789da6e287c16
Priti Mahadik <mahadik_priti@yahoo.com>
2025-05-06 23:13:35 +0100

    AAD Service Principal 1

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -18,6 +18,26 @@ variable "subscription_id" {
   type        = string
 }
 
+variable "subscription_id" {
+  description = "Azure Subscription ID to grant to the SP"
+  type        = string
+}
+
+variable "github_token" {
+  description = "GitHub PAT with repo & workflow scopes"
+  type        = string
+}
+
+variable "github_owner" {
+  description = "GitHub org/user name"
+  type        = string
+}
+
+variable "github_repository" {
+  description = "GitHub repo name (e.g. ado-githubactions-terraform)"
+  type        = string
+}
+
 #
 variable "rg_name"     { default = "adogithubtf-rg" }
 variable "location"    { default = "uksouth" }
