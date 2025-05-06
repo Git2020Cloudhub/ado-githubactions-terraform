2b0082a46ff000e107a9aab51cc783f9c63c81c8
Priti Mahadik <mahadik_priti@yahoo.com>
2025-05-07 00:38:54 +0100

    revise8

--- terraform/storage.tf
+++ terraform/storage.tf
@@ -1,11 +1,11 @@
-resource "azurerm_storage_account" "state" {
-  #name                     = "tfstate${replace(var.rg_name, "/-/", "")}"
-  name                     = "tfstateadogithubaction" 
-  resource_group_name      = azurerm_resource_group.rg.name
-  location                 = azurerm_resource_group.rg.location
-  account_tier             = "Standard"
-  account_replication_type = "LRS"
-}
+ resource "azurerm_storage_account" "state" {
+   name                = "tfstateadogithubaction"      # must exactly match backend.tf
+   resource_group_name = azurerm_resource_group.rg.name
+   location            = azurerm_resource_group.rg.location
+   account_tier        = "Standard"
+   account_replication_type = "LRS"
+ }
+
 
 resource "azurerm_storage_container" "state" {
   name                  = "state"
